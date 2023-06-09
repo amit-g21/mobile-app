eabbbdbfb34469ecd19e9509e11674fe8d807834
hilma <amitgonen2468@gmail.com>
2023-06-09 15:45:16 +0300

    add some work good version

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,49 @@
-import { View , Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native';
+import { View, Text, StatusBar, Platform, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { styles } from '../theme';
+import TrendingMovies from '../components/TrendingMovies';
+import MovieList from '../components/MovieList';
 
-export default function HomeScreen(){
+const ios = Platform.OS === 'ios'
+
+export default function HomeScreen() {
+
+    const [trending, setTrending] = useState([1, 2, 3])
+    const [upcoming, setUpcoming] = useState([1, 2, 3])
+    const [topRated, setTopRated] = useState([1, 2, 3])
+
+
     return (
-        <SafeAreaView>
-            <Text>Home screen</Text>
-        </SafeAreaView>
+        <View className='flex-1 bg-neutral-950'>
+            {/* search bar and logo*/}
+            <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
+                <StatusBar style='light' />
+                <View className='flex-row justify-between items-center mx-4'>
+                    <Bars3CenterLeftIcon size="30" strokeWidth={2} color='white' />
+                    <Text
+                        className="text-white text-3xl font-bold">
+                        <Text style={styles.text}>M</Text>ovies
+                    </Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size='30' strokeWidth={2} color='white' />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+            <ScrollView
+                indicatorStyle={{backgroundColor: 'white'}}
+                showsVerticalScrollIndicator={ true}
+                contentContainerStyle={{ paddingBottom: 10 }}
+            >
+
+            {/* trending movie carousel*/}
+            <TrendingMovies data={trending} />
+
+            {/* upcoming movie row*/}
+            <MovieList title='Upcoming' data={upcoming} />
+
+        </ScrollView>
+        </View >
     )
 }
